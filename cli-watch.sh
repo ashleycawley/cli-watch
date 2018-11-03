eab129cc3c5ab86bd9efe3100f105470aa30672e
Ashley Cawley <ash@ashleycawley.co.uk>
2018-11-03 18:54:17 +0000

    Took out redundant code near the bottom

--- cli-watch.sh
+++ cli-watch.sh
@@ -62,13 +62,13 @@ do
         fi
 
 
-    if [ `md5sum /home/ashley/.bash_history` == `md5sum $WORKINGDIR/.bash_history_working` ]
-    then
+    #if [ `md5sum /home/ashley/.bash_history` == `md5sum $WORKINGDIR/.bash_history_working` ]
+    #then
 
-        echo "User's .bash_history matches our working copy so no further work is needed, exiting..." # DEBUGGING
-        exit 0
+        #echo "User's .bash_history matches our working copy so no further work is needed, exiting..." # DEBUGGING
+        #exit 0
 
-    else
+    #else
 
     # Differences found between user's .bash_history and our working copy
     # Contiune with script to compare changes and alert people
@@ -81,7 +81,7 @@ do
 
 
 
-    fi
+    #fi
 
 
 
@@ -91,35 +91,3 @@ done
 
 
 exit 0
-
-#############################################
-
-
-TMPDIR=/home/cli-watch # No trailing slash
-USERLIST=(`ls /home/ | cat`)
-# Tests to see if temp directory exists and if it does not then it creates it
-if [ ! -d "$TMPDIR" ]
-then
-    # Create temp directory and restricts permissions
-    mkdir -p $TMPDIR
-    chmod 600 $TMPDIR
-    mkdir -p $TMPDIR/users/
-    chmod 600 $TMPDIR/users/
-    
-fi
-
-# Backing up the delimiter (usually a space) used by arrays to differentiate between different data in the array
-SAVEIFS=$IFS
-
-# Change the delimiter used by arrays from a space to a new line, this allows a list of users (on new lines) to be stored in to an array
-IFS=$'\n'
-
-USERARRAY=$($USERLIST)
-
-if [ ! -d "$TMPDIR/users/" ]
-then
-    # Create temp directory and restricts permissions
-    mkdir -p $TMPDIR
-    chmod 600 $TMPDIR
-fi
-mkdir -p 
