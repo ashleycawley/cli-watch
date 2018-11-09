9357f2f0031cfd2b9edb6ff6c69a5106aea57b70
Ashley Cawley <ash@ashleycawley.co.uk>
2018-11-09 15:04:32 -0500

    Added support for root, checking differences and handling log formatting

--- cli-watch.sh
+++ cli-watch.sh
@@ -31,17 +31,26 @@ do
     then
         # Tests for differences between users bash history and our working copy, it saves new/differences into variable $DIFFERENCES
         DIFFERENCES=`diff -u0 /home/$USER/.bash_history $WORKINGDIR/$USER.bash_history_working | grep -v -E "^/home/(swb|ansible)" | grep -v "\---" | grep -v "@" | cut -c 2- | grep -v "+"`
+	ROOTDIFFERENCES=`diff -u0 /home/root/.bash_history $WORKINGDIR/root.bash_history_working | grep -v "\---" | grep -v "@" | cut -c 2- | grep -v "+"`
 
         for COMMAND in `echo "$IMMEDIATECOMMANDS"`
         do
             # Tests to see if any of the recent commands include commands of interest and stores it in a file called USERNAME.hits
-            echo "$DIFFERENCES" | grep -i "$COMMAND" >> $WORKINGDIR/$USER.hits
+            echo "$DIFFERENCES" | grep -i "$COMMAND" >> $WORKINGDIR/$USER.hits && DIFFHIT="1"
+	    echo "$ROOTDIFFERENCES" | grep -i "$COMMAND" >> $WORKINGDIR/root.hits && ROOTDIFFHIT="1"
 
             # Only if the command above found hits does it re-write the log format
-            if [ `echo $?` == "0" ]
+            if [ $DIFFHIT == "1" ]
             then
                 # Applies cli-watch log formatting to hits found so far
                 sed -i -e "s,^,`date +'%d-%m-%Y %H:%M'` `hostname` cli-watch [$USER] ,g" $WORKINGDIR/$USER.hits 2>/dev/null
+            fi
+
+	    # The same as above but for the root user
+ 	    if [ $ROOTDIFFHIT == "1" ]
+            then
+                # Applies cli-watch log formatting to hits found so far
+                sed -i -e "s,^,`date +'%d-%m-%Y %H:%M'` `hostname` cli-watch [root] ,g" $WORKINGDIR/root.hits 2>/dev/null
             fi
 
             # Checks to see if there are any hits to report and if there is it dispatches the alert email
